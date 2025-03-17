9591fbd550d1a76e0c1d9a99142e257bf6d92d33
Earl Lamier <77307962+earlblamier@users.noreply.github.com>
2025-03-17 05:26:55 -0400

    update

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'],
+  imports: [FormsModule]
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  login() {
+    // Implement your login logic here
+    console.log('Username:', this.username);
+    console.log('Password:', this.password);
+  }
 }
